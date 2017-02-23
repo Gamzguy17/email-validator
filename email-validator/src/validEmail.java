ebe3c53f5894a05913370d7c42452c5a52470e18
Grant <vr867260@dal.ca>
2017-02-23 18:14:22 -0400

    First Draft
    Testing file upload directly on GUI GitHub

---
+++ email-validator/src/validEmail.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class validEmail {
+	
+	public static void main(String [] args){
+		Scanner keyboard = new Scanner(System.in);
+		String userInput;
+		System.out.print("Please type in an email address: ");
+		userInput = keyboard.nextLine(); 
+	}
+	public boolean validate(String i){
+		return true;
+	}
+}
